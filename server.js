15d8780a545e08685aa97135b4ffe16f696dad71
Matt Pityo <mpityo@gmail.com>
2022-03-05 21:27:40 -0500

    add routes and set up base server

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-const app = require('express');
+const express = require('express');
+
+const htmlRoutes = require('./routes/htmlRoutes');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// makes all public assets available for all pages
+app.use(express.static('public'));
+// main index
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+    console.log(`API server now on port  ${PORT}`);
+});
