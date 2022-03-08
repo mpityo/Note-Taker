124fe431da97e8562a824ed75e2b33b2cb39d950
Matt Pityo <mpityo@gmail.com>
2022-03-07 19:37:19 -0500

    send note back to add to db file

--- routes/apiRoutes/notes.js
+++ routes/apiRoutes/notes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const path = require('path');
 const { nanoid } = require('nanoid');
 const notesArr = require('../../db/db.json');
+const { createNewNote } = require("../../lib/notes");
 
 router.get('/notes', (req, res) => {
     let results = notesArr;
@@ -11,8 +12,8 @@ router.get('/notes', (req, res) => {
 router.post("/notes", (req, res) => {
     req.body.id = nanoid();
 
-    notesArr.push(req.body);
-    res.json(notesArr);
+    const notes = createNewNote(req.body, notesArr);
+    res.json(notes);
 });
 
 module.exports = router;
