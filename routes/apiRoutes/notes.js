49eb71d53c46f119a6e82297d5ddfdfaab72acb3
Matt Pityo <mpityo@gmail.com>
2022-03-09 21:53:40 -0500

    add delete functionality for notes

--- routes/apiRoutes/notes.js
+++ routes/apiRoutes/notes.js
@@ -2,16 +2,21 @@ const router = require('express').Router();
 const path = require('path');
 const { nanoid } = require('nanoid');
 const notesArr = require('../../db/db.json');
-const { createNewNote } = require("../../lib/notes");
+const { createNewNote, deleteNote } = require("../../lib/notes");
 
 router.get('/notes', (req, res) => {
     res.json(notesArr);
 });
 
-router.post("/notes", (req, res) => {
+router.post('/notes', (req, res) => {
     req.body.id = nanoid();
 
     const notes = createNewNote(req.body, notesArr);
+    res.json(notes);
+});
+
+router.delete('/notes/:id', (req, res) => {
+    const notes = deleteNote(req.params.id, notesArr);
     res.json(notes);
 });
 
